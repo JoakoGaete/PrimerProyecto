085fc807535c475c632b9acfbf9ee10b1c37072d
unknown <SSDD@PNAWSA05LC52221.duoc.acad>
2025-05-06 14:36:33 -0400

    Primer guardado de PrimerProyecto

---
+++ demo/src/test/java/PrimerProyecto/demo/PrimerProyectoApplicationTests.java
@@ -0,0 +1,13 @@
+package PrimerProyecto.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrimerProyectoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
